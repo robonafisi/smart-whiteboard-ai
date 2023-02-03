a66c1fb34f28f05c79de66282a61de78cb9d99de
Mohammad Nafisi <nafisidev@gmail.com>
2023-02-04 00:30:41 +0800

    Basic components are working

---
+++ src/components/drawboard.tsx
@@ -0,0 +1,64 @@
+import { Excalidraw } from "@excalidraw/excalidraw"
+import { useState, useEffect } from 'react'
+
+
+export default function Drawboard(props) {
+  const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+
+  const updateScene = () => {
+    const sceneData = {
+      elements: [
+        {
+      "id": "ppzhQk8nnTjXQbjjx78ph",
+      "type": "text",
+      "x": 300,
+      "y": 91,
+      "width": 700,
+      "height": 150,
+      "angle": 0,
+      "strokeColor": "#000000",
+      "backgroundColor": "transparent",
+      "fillStyle": "hachure",
+      "strokeWidth": 1,
+      "strokeStyle": "solid",
+      "roughness": 1,
+      "opacity": 100,
+      "groupIds": [],
+      "roundness": null,
+      "seed": 543111480,
+      "version": 74,
+      "versionNonce": 488114504,
+      "isDeleted": false,
+      "boundElements": null,
+      "updated": 1675504695013,
+      "link": null,
+      "locked": false,
+      "text": props.text,
+      "fontSize": 10,
+      "fontFamily": 1,
+      "textAlign": "left",
+      "verticalAlign": "top",
+      "baseline": 18,
+      "containerId": null,
+      "originalText": props.text
+        },
+      ],
+      appState: {
+        viewBackgroundColor: "#edf2ff",
+      },
+    };
+    excalidrawAPI.updateScene(sceneData);
+  };
+
+  return (
+    <>
+      <div>
+        <h1 class='text-red-600'>Excalidraw Example</h1>
+        <button onClick={updateScene}>Update Page</button>
+        <div style={{ height: "500px" }}>
+          <Excalidraw ref={(api) => setExcalidrawAPI(api)} />
+        </div>       
+      </div>
+    </>
+  );
+}
